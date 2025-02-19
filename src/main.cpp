6b2cb00a7bcc9ff9e1a5411f330a449bf0943968
chenota <achenot@mines.edu>
2025-02-18 22:58:30 -0700

    feat: Read input into matricies in column-major order (yuck)

--- src/main.cpp
+++ src/main.cpp
@@ -33,17 +33,40 @@ int main(int argc, char *argv[]) {
         std::cerr << "Expected positional arguments Text and Key" << std::endl;
         exit(1);
     }
+    // Cast text and key to strings
+    std::string text_str = argv[optind];
+    std::string key_str = argv[optind];
+    // Buffers to hold strings for text and key
+    char text_buf[32] = { '0' };
+    char key_buf[32] = { '0' };
     // Validate length
-    if(strnlen(argv[optind], 33) > 32 || strnlen(argv[optind + 1], 33) > 32) {
+    if(text_str.length() > 32 || key_str.length() > 32) {
         std::cerr << "Text and Key length must be no more than 32 characters" << std::endl;
         exit(1);
     }
+    // Fill buffers
+    for(size_t i = 0; i < text_str.length(); i++) text_buf[(32 - text_str.length()) + i] = text_str[i];
+    for(size_t i = 0; i < key_str.length(); i++) key_buf[(32 - key_str.length()) + i] = key_str[i];
+    // Hexit buffer
+    char hexit_buf[3] = "00";
     // Allocate text and key blocks
-    uint64_t text = 0;
-    uint64_t key = 0;
+    uint8_t text[4][4] = { 0 };
+    uint8_t key[4][4] = { 0 };
     // Fill blocks
-    text = strtoull(argv[optind], NULL, 32);
-    key = strtoull(argv[optind + 1], NULL, 32);
-    // Exit success
+    for(size_t i = 0; i < 16; i++) {
+        // Fill hexit buffer
+        hexit_buf[0] = text_buf[i * 2];
+        hexit_buf[1] = text_buf[(i * 2) + 1];
+        // Fill value from hexit buffer
+        text[i % 4][i / 4] = strtoul(hexit_buf, NULL, 16);
+    }
+    // Fill blocks
+    for(size_t i = 0; i < 16; i++) {
+        // Fill hexit buffer
+        hexit_buf[0] = key_buf[i * 2];
+        hexit_buf[1] = key_buf[(i * 2) + 1];
+        // Fill value from hexit buffer
+        key[i % 4][i / 4] = strtoul(hexit_buf, NULL, 16);
+    }
     return 0;
 }
