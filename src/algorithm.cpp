6dd503b7549e9e468b071500f80a58ebfd8fa24b
chenota <achenot@mines.edu>
2025-02-23 15:09:49 -0700

    fix: Fixed incorrect fastMultiply

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -31,26 +31,39 @@ void shiftRows(uint8_t bytes[4][4]) {
     bytes[3][3] = bytes_copy[2][3];
 }
 
-uint8_t fastMultiply(uint8_t x, uint8_t n) {
-    // Base case: If multiplying by n=0 (x^0), return x
+uint8_t fastExponent(uint8_t x, uint8_t n) {
+    // Base case: If multiplying by n=0 return x
     if(n == 0) return x;
     // Recursive case
-    uint8_t x_new = fastMultiply(x, n-1);
+    uint8_t x_new = fastExponent(x, n-1);
     // Check if b7 is zero
-    if(x_new & 0x80 == 0) {
-        return x_new < 1;
+    if((x_new & 0x80) == 0) {
+        return x_new << 1;
+        
     } else {
-        return (x_new < 1) ^ 0x1B;
+        return (x_new << 1) ^ 0x1B;
     }
 }
 
+uint8_t fastMultiply(uint8_t x, uint8_t n) {
+    // Result
+    uint8_t result = 0;
+    // Loop through each bit
+    for(uint8_t i = 0; i < 8; i++) if((n >> i) & 1) result ^= fastExponent(x, i);
+    // Return
+    return result;
+}
+
 void mixColumns(uint8_t bytes[4][4]) {
+    // Make a copy of the bytes matrix
+    uint8_t bytes_copy[4][4];
+    std::copy(&bytes[0][0], &bytes[0][0]+16, &bytes_copy[0][0]);
     // For each of the four columns...
     for(uint8_t i = 0; i < 4; i++) {
-        bytes[i][0] = fastMultiply(bytes[i][0], 2) ^ fastMultiply(bytes[i][1], 3) ^ fastMultiply(bytes[i][2], 1) ^ fastMultiply(bytes[i][3], 1);
-        bytes[i][1] = fastMultiply(bytes[i][0], 1) ^ fastMultiply(bytes[i][1], 2) ^ fastMultiply(bytes[i][2], 3) ^ fastMultiply(bytes[i][3], 1);
-        bytes[i][2] = fastMultiply(bytes[i][0], 1) ^ fastMultiply(bytes[i][1], 1) ^ fastMultiply(bytes[i][2], 2) ^ fastMultiply(bytes[i][3], 3);
-        bytes[i][3] = fastMultiply(bytes[i][0], 3) ^ fastMultiply(bytes[i][1], 1) ^ fastMultiply(bytes[i][2], 1) ^ fastMultiply(bytes[i][3], 2);
+        bytes[i][0] = fastMultiply(bytes_copy[i][0], 2) ^ fastMultiply(bytes_copy[i][1], 3) ^ fastMultiply(bytes_copy[i][2], 1) ^ fastMultiply(bytes_copy[i][3], 1);
+        bytes[i][1] = fastMultiply(bytes_copy[i][0], 1) ^ fastMultiply(bytes_copy[i][1], 2) ^ fastMultiply(bytes_copy[i][2], 3) ^ fastMultiply(bytes_copy[i][3], 1);
+        bytes[i][2] = fastMultiply(bytes_copy[i][0], 1) ^ fastMultiply(bytes_copy[i][1], 1) ^ fastMultiply(bytes_copy[i][2], 2) ^ fastMultiply(bytes_copy[i][3], 3);
+        bytes[i][3] = fastMultiply(bytes_copy[i][0], 3) ^ fastMultiply(bytes_copy[i][1], 1) ^ fastMultiply(bytes_copy[i][2], 1) ^ fastMultiply(bytes_copy[i][3], 2);
     }
 }
 
